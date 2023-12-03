4792044078f1b7b9d1095fe9798b82d2963f30ee
ela-rami <elarami4real@gmail.com>
2023-12-03 02:30:59 +0100

    homescreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -14,67 +15,71 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF1E1E1E),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start, // Allinea gli elementi a sinistra
-          children: [
-            Row(
-              children: [
-                Icon(
-                  Icons.work,
-                  color: Color(0xFF39FF14),
-                  size: 36,
-                ),
-                SizedBox(width: 10), // Spazio tra icona e testo
-                Text(
-                  'Last Workout:',
-                  style: TextStyle(
-                    color: Color(0xFF39FF14),
-                    fontSize: 18,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 24), // Spazio tra le righe
-            Row(
-              children: [
-                Icon(
-                  Icons.timer,
-                  color: Color(0xFF39FF14),
-                  size: 36,
-                ),
-                SizedBox(width: 10), // Spazio tra icona e testo
-                Text(
-                  'Tot Time:',
-                  style: TextStyle(
-                    color: Color(0xFF39FF14),
-                    fontSize: 18,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 24), // Spazio tra le righe
-            Row(
-              children: [
-                Icon(
-                  Icons.star,
-                  color: Color(0xFF39FF14),
-                  size: 36,
-                ),
-                SizedBox(width: 10), // Spazio tra icona e testo
-                Text(
-                  'Streak:',
-                  style: TextStyle(
-                    color: Color(0xFF39FF14),
-                    fontSize: 18,
-                  ),
-                ),
-              ],
-            ),
-          ],
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildRow('assets/icons/back.svg', 'Last Workout:', '45 min'),
+              SizedBox(height: 24),
+              buildRow('assets/icons/timer.svg', 'Tot Time:', '1h 30 min'),
+              SizedBox(height: 24),
+              buildRow('assets/icons/fire.svg', 'Streak:', '5 days'),
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  Widget buildRow(String iconPath, String text1, String text2) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Expanded(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset(
+                iconPath,
+                color: Color(0xFF39FF14),
+                width: 32,
+                height: 32,
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                text1,
+                style: TextStyle(
+                  color: Color.fromARGB(255, 255, 255, 255),
+                  fontSize: 18,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                text2,
+                style: TextStyle(
+                  color: Color.fromARGB(255, 255, 255, 255),
+                  fontSize: 18,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
