82b9c820d181eabeacb5b28e5a80fba7377c4bac
ela-rami <elarami4real@gmail.com>
2023-12-06 15:51:41 +0100

    homescreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, deprecated_member_use
 
+import 'package:fit_flow/screens/workout_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -16,12 +17,34 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       backgroundColor: Color(0xFF1E1E1E),
       body: Padding(
-        padding: EdgeInsets.all(16.0),
+        padding: EdgeInsets.all(16.32),
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => WorkoutScreen()));
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Color(0xFF39FF14), // Colore di sfondo
+                  onPrimary: Color(0xFF1E1E1E), // Colore del testo
+                  minimumSize: Size.fromHeight(100), // Altezza fissa di 100
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20), // Corner radius di 20
+                  ),
+                ),
+                child: Text(
+                  'Create Workout',
+                  style: TextStyle(
+                    fontSize: 32, 
+                    fontWeight: FontWeight.bold, 
+                    fontFamily: 'Lato', 
+                  ),
+                ),
+              ),
+              SizedBox(height: 100),
               buildRow('assets/icons/back.svg', 'Last Workout:', '45 min'),
               SizedBox(height: 24),
               buildRow('assets/icons/timer.svg', 'Tot Time:', '1h 30 min'),
